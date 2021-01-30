8ee10c354cdfdb7eeb97c18c9fa48291c2e7b08c
Kotarosz727 <kotarosz727@gmail.com>
2021-01-30 19:41:37 +0900

    add expo-speech

--- components/Card.js
+++ components/Card.js
@@ -1,9 +1,22 @@
 import React from "react";
-import { Card, ListItem, Button, Icon } from "react-native-elements";
+import {
+  Card,
+  ListItem,
+  Button,
+  Icon,
+  TouchableOpacity,
+} from "react-native-elements";
 import { StyleSheet, Text, View, FlatList } from "react-native";
 import Icon5 from "react-native-vector-icons/FontAwesome5";
+import * as Speech from "expo-speech";
 
 export default function card({ data }) {
+  const speak = () => {
+    Speech.speak("Hello World!");
+  };
+  const test = () => {
+    console.log(123)
+  }
   const bookmark = (
     <>
       <Icon5 name="bookmark" size={25} />
@@ -17,7 +30,7 @@ export default function card({ data }) {
     return (
       <>
         <Text style={{ marginBottom: 15, top: 0 }}>{speaker}</Text>
-        <Text style={styles.content}>{item.chinese}</Text>
+        <Text style={styles.content} onPress={()=>Speech.speak(item.chinese, {language:'zh'})}>{item.chinese}</Text>
         <Text style={styles.pinin}>{item.pinin}</Text>
         <Text style={styles.bookmark}>{bookmark}</Text>
       </>
@@ -59,6 +72,6 @@ const styles = StyleSheet.create({
     // position: "absolute",
     bottom: 0,
     left: 0,
-    marginTop: 10
+    marginTop: 10,
   },
 });
