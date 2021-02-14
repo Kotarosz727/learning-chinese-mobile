d675594e64a21fee3e65b76e11d9519f7510f4db
Kotarosz727 <kotarosz727@gmail.com>
2021-02-14 19:11:29 +0900

    remove unused library

--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -1,33 +1,33 @@
-import React, { useState, useEffect } from "react";
-import { Text, View, StyleSheet, TextInput } from "react-native";
-import { Button, Overlay, Input } from "react-native-elements";
+import React from "react";
+import { Text, View } from "react-native";
+import { Button } from "react-native-elements";
 
 export default function HomeScreen({ navigation, username }) {
-    return (
-      <View style={{ flex: 1, alignItems: "center" }}>
-        <View style={{ marginTop: 150 }}>
-          {username ? (
-            <Text style={{ marginBottom: 50, fontSize: 25 }}>
-              您好!{username}
-            </Text>
-          ) : (
-            <Text></Text>
-          )}
-        </View>
-        <Button
-          title="中国語 Level①"
-          onPress={() => navigation.navigate("Level1")}
-          type="outline"
-          raised={true}
-          containerStyle={{ marginBottom: 40, width: 300 }}
-        />
-        <Button
-          title="中国語 Level②"
-          onPress={() => navigation.navigate("Level2")}
-          type="outline"
-          raised={true}
-          containerStyle={{ marginBottom: 40, width: 300 }}
-        />
+  return (
+    <View style={{ flex: 1, alignItems: "center" }}>
+      <View style={{ marginTop: 150 }}>
+        {username ? (
+          <Text style={{ marginBottom: 50, fontSize: 25 }}>
+            您好!{username}
+          </Text>
+        ) : (
+          <Text></Text>
+        )}
       </View>
-    );
-  }
+      <Button
+        title="中国語 Level①"
+        onPress={() => navigation.navigate("Level1")}
+        type="outline"
+        raised={true}
+        containerStyle={{ marginBottom: 40, width: 300 }}
+      />
+      <Button
+        title="中国語 Level②"
+        onPress={() => navigation.navigate("Level2")}
+        type="outline"
+        raised={true}
+        containerStyle={{ marginBottom: 40, width: 300 }}
+      />
+    </View>
+  );
+}
