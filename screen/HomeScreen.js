7bf3f0118dda6e891b7677455e1b13e425fc7588
Kotarosz727 <kotarosz727@gmail.com>
2021-03-05 15:57:44 +0900

    hide 基本120文

--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -5,17 +5,17 @@ import { Button } from "react-native-elements";
 export default function HomeScreen({ navigation, username }) {
   return (
     <View style={{ flex: 1, alignItems: "center" }}>
-      <Button
+      {/* <Button
         title="基本120文"
         onPress={() => navigation.navigate("content", { query: "120" })}
         raised={true}
         containerStyle={{ marginTop: 100, marginBottom: 40, width: 300 }}
-      />
+      /> */}
       <Button
         title="日常会話"
         onPress={() => navigation.navigate("content", { query: "nichijo" })}
         raised={true}
-        containerStyle={{ marginBottom: 40, width: 300 }}
+        containerStyle={{ marginTop: 100, marginBottom: 40, width: 300 }}
       />
       <Button
         title="動詞"
